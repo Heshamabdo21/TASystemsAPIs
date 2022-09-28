e90b4dabd51f96549557ea1ab9cdb2094cd0e6e6
Heshamabdo21 <heshamabdo21@gmail.com>
2022-09-28 15:00:42 +0200

    Main Project
    Main Project

--- src/test/java/SteeringCompanyAPI_TestCases/Test_Case.java
+++ src/test/java/SteeringCompanyAPI_TestCases/Test_Case.java
@@ -14,33 +14,76 @@ public class Test_Case {
     	Token_API Token_TC=new Token_API();
     	String Token =Token_TC.POST_TOKEN_Rq();
     	Lookups_API Lookups_TC=new Lookups_API();
+    }
+    @Test
+    public void Valid_GET_all_cities_Lookups_Rq_TC() {
+    	Token_API Token_TC=new Token_API();
+    	String Token =Token_TC.POST_TOKEN_Rq();
+    	Lookups_API Lookups_TC=new Lookups_API();
     	Lookups_TC.GET_all_cities_Lookups_Rq(Token);
     	Lookups_TC.GET_all_cities_Lookups_by_parameter_Query_Rq(Token,"3","0");
-    	
+    }
+    @Test
+    public void Valid_GET_all_hotels_Lookups_Rq_TC() {
+    	Token_API Token_TC=new Token_API();
+    	String Token =Token_TC.POST_TOKEN_Rq();
+    	Lookups_API Lookups_TC=new Lookups_API();
     	Lookups_TC.GET_all_hotels_Lookups_Rq(Token);
     	Lookups_TC.GET_all_hotels_Lookups_by_parameter_Query_Rq(Token,"3","0");
-    	
+    }
+    
+    @Test
+    public void Valid_GET_all_routes_Lookups_Rq_TC() {
+    	Token_API Token_TC=new Token_API();
+    	String Token =Token_TC.POST_TOKEN_Rq();
+    	Lookups_API Lookups_TC=new Lookups_API();
     	Lookups_TC.GET_all_routes_Lookups_Rq(Token);
     	Lookups_TC.GET_all_routes_Lookups_by_parameter_Query_Rq(Token,"3","0");
-
+    }
+    
+    @Test
+    public void Valid_GET_all_sectors_Lookups_Rq_TC() {
+    	Token_API Token_TC=new Token_API();
+    	String Token =Token_TC.POST_TOKEN_Rq();
+    	Lookups_API Lookups_TC=new Lookups_API();
     	Lookups_TC.GET_all_sectors_Lookups_Rq(Token);
     	Lookups_TC.GET_all_sectors_Lookups_by_parameter_Query_Rq(Token,"3","0");
-
-    	
+    }
+    @Test
+    public void Valid_GET_all_ports_Lookups_Rq_TC() {
+    	Token_API Token_TC=new Token_API();
+    	String Token =Token_TC.POST_TOKEN_Rq();
+    	Lookups_API Lookups_TC=new Lookups_API();
     	Lookups_TC.GET_all_ports_Lookups_Rq(Token);
     	Lookups_TC.GET_all_ports_Lookups_by_parameter_Query_Rq(Token,"3","0");
-
+    }
+    
+    @Test
+    public void Valid_GET_all_terminals_Lookups_Rq_TC() {
+    	Token_API Token_TC=new Token_API();
+    	String Token =Token_TC.POST_TOKEN_Rq();
+    	Lookups_API Lookups_TC=new Lookups_API();
     	Lookups_TC.GET_all_terminals_Lookups_Rq(Token);
     	Lookups_TC.GET_all_terminals_Lookups_by_parameter_Query_Rq(Token,"3","0");
-
+    }
+    
+    @Test
+    public void Valid_GET_all_vehicleTypes_Lookups_Rq_TC() {
+    	Token_API Token_TC=new Token_API();
+    	String Token =Token_TC.POST_TOKEN_Rq();
+    	Lookups_API Lookups_TC=new Lookups_API();
     	Lookups_TC.GET_all_vehicleTypes_Lookups_Rq(Token);
     	Lookups_TC.GET_all_vehicleTypes_Lookups_by_parameter_Query_Rq(Token,"3","0");
-
+    }
+    @Test
+    public void Valid_GET_all_vehicleCategories_Lookups_Rq_TC() {
+    	Token_API Token_TC=new Token_API();
+    	String Token =Token_TC.POST_TOKEN_Rq();
+    	Lookups_API Lookups_TC=new Lookups_API();
     	Lookups_TC.GET_all_vehicleCategories_Lookups_Rq(Token);
     	Lookups_TC.GET_all_vehicleCategories_Lookups_by_parameter_Query_Rq(Token,"3","0");
-
-
-
     }
+
+
 	
 }
