2a9cc843a23c9ca737bdb208c9c3fadd37d2dcf0
Heshamabdo21 <heshamabdo21@gmail.com>
2022-12-19 12:46:47 +0200

    test

--- generate_allure_report.bat
+++ generate_allure_report.bat
@@ -1,5 +1,8 @@
 @echo off
-set path=C:\Users\HeshamAbdElhamedGhar\.m2\repository\allure\allure-2.19.0\bin;C:\Program Files\Java\jdk-17.0.2\bin;%path%
+:: If you already have a valid JAVA_HOME environment variable set, feel free to comment the below two lines
+set JAVA_HOME=C:\Program Files\Java\jdk-17.0.2
+set path=%JAVA_HOME%\bin;%path%
+set path=C:\Users\HeshamAbdElhamedGhar\.m2\repository\allure\allure-2.20.1\bin;%path%
 allure serve allure-results
 pause
 exit
