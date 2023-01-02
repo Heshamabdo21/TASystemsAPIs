37fc4945bca2ec94c0c50d2ed17f70fd39fb9dac
Heshamabdo21 <heshamabdo21@gmail.com>
2023-01-02 17:45:58 +0200

    test

--- src/main/java/SteeringCompanyAPIs/Policies_API.java
+++ src/main/java/SteeringCompanyAPIs/Policies_API.java
@@ -2,6 +2,7 @@ package SteeringCompanyAPIs;
 
 import com.shaft.driver.SHAFT;
 import com.shaft.driver.SHAFT.API;
+import com.shaft.tools.io.ExcelFileManager;
 import com.shaft.api.*;
 import com.shaft.api.RequestBuilder.AuthenticationType;
 
@@ -14,63 +15,66 @@ import java.util.List;
 
 
 public class Policies_API {
+    ExcelFileManager testDataReader = new ExcelFileManager("SteeringCompanyAPI_TestData/SteeringCompanyAPI_TestData.xlsx");
+    String BaseURL = testDataReader.getCellData("API_Data","Steering_Base_URL","Data1");
 
-    String BaseURL = "https://api-demo.np.transporticonline.com/steeringcompanies/v1" ;
+
+    //String BaseURL = "https://api-demo.np.transporticonline.com/steeringcompanies/v1" ;
    
-    public void GET_all_Lookups_policies_Rq(String TokenValue) {
-        String Lookup_policies_Path = "/policies";
+    Response All_Policies_Response;
+    SHAFT.API All_Policies_api;
+   // String All_Policies_Path = "/policies";
+    String All_Policies_Path = testDataReader.getCellData("API_Data","GetAllPolicies","Data1");
+  
+    public void GET_All_Policies_Rq(String TokenValue) {
+     //   String Lookup_policies_Path = "/policies";
 
-    	SHAFT.API Lookups_policies_api = new SHAFT.API(BaseURL);
-    
-    	Lookups_policies_api.get(Lookup_policies_Path).
+    	 All_Policies_api = new SHAFT.API(BaseURL); 
+    	 All_Policies_api.get(All_Policies_Path).
     	setAuthentication("","", AuthenticationType.NONE).
-    	addHeader("Authorization", "Bearer " + TokenValue).perform();
-
+    	addHeader("Authorization", "Bearer " + TokenValue).perform();     
+         All_Policies_Response = All_Policies_api.getResponse();
+    }
+    
+    public void Check_Valid_policies_status_Code_Response(){
         
-        Response Lookups_policies_Response = Lookups_policies_api.getResponse();
-        
-        SHAFT.Validations.assertThat().number(Lookups_policies_Response.getStatusCode()).isEqualTo(200).perform();
-        SHAFT.Validations.verifyThat().number(Lookups_policies_Response.getTime()).isGreaterThanOrEquals(1.1).perform();
-        SHAFT.Validations.verifyThat().number(Lookups_policies_Response.getTime()).isLessThanOrEquals(10000).perform();
-        String Lookups_policies_ResponseBody = Lookups_policies_Response.getBody().asString();
+        SHAFT.Validations.assertThat().number(All_Policies_Response.getStatusCode()).isEqualTo(200).perform();
+    }
+    
+    public void Check_Valid_policies_Response_Time() {
+    
+        SHAFT.Validations.verifyThat().number(All_Policies_Response.getTime()).isGreaterThanOrEquals(1.1).perform();
+        SHAFT.Validations.verifyThat().number(All_Policies_Response.getTime()).isLessThanOrEquals(10000).perform();
+    }
+    
+    public void Check_policies_Valid_Content() {
+        String Lookups_policies_ResponseBody = All_Policies_Response.getBody().asString();
         SHAFT.Validations.assertThat().object(Lookups_policies_ResponseBody).contains("cancellationPolicies").perform();
-       
-        Lookups_policies_api.assertThatResponse().extractedJsonValue("cancellationPolicies").isNotNull().withCustomReportMessage("Check that cancellationPolicies object is not null.").perform();
-        
-        Lookups_policies_api.assertThatResponse().matchesSchema("Lookups_policies_schema.json").perform();
+   
+    
+        All_Policies_api.assertThatResponse().extractedJsonValue("cancellationPolicies").isNotNull().withCustomReportMessage("Check that cancellationPolicies object is not null.").perform();
+    }
+    
+    public void Check_policies_Response_Valid_Schema() {
+        All_Policies_api.assertThatResponse().matchesSchema(testDataReader.getCellData("API_Data","GetAllPolicies","Data2")).perform();
      
     }
    
-    public void GET_all_Lookups_policies_by_parameter_Query_Rq(String TokenValue,String PageSize,String PageNumber) {        
-        String Lookup_policies_Path = "/policies";
-    	SHAFT.API Lookups_policies_api = new SHAFT.API(BaseURL);
+    public void GET_All_Policies_Path_by_parameter_Query_Rq(String TokenValue,String PageSize,String PageNumber) {        
+      //  String Lookup_policies_Path = "/policies";
+        All_Policies_api = new SHAFT.API(BaseURL);
 
     	List<List<Object>> parameters = Arrays.asList(Arrays.asList("size", PageSize), 
     			Arrays.asList("page",PageNumber));
 
-    	Lookups_policies_api.get(Lookup_policies_Path).
+    	All_Policies_api.get(All_Policies_Path).
     	setAuthentication("", "", AuthenticationType.NONE).
     	setParameters(parameters, RestActions.ParametersType.QUERY).
     	addHeader("Authorization", "Bearer " + TokenValue).perform();
 
         
-        Response Lookups_policies_Response = Lookups_policies_api.getResponse();
+    	All_Policies_Response = All_Policies_api.getResponse();
         
-        SHAFT.Validations.assertThat().number(Lookups_policies_Response.getStatusCode()).isEqualTo(200).perform();
-        SHAFT.Validations.verifyThat().number(Lookups_policies_Response.getTime()).isGreaterThanOrEquals(1.1).perform();
-        SHAFT.Validations.verifyThat().number(Lookups_policies_Response.getTime()).isLessThanOrEquals(10000).perform();
-        String Lookups_policies_ResponseBody = Lookups_policies_Response.getBody().asString();
-        SHAFT.Validations.assertThat().object(Lookups_policies_ResponseBody).contains("cancellationPolicies").perform();
-       
-        Lookups_policies_api.assertThatResponse().extractedJsonValue("cancellationPolicies").isNotNull().withCustomReportMessage("Check that cancellationPolicies object is not null.").perform();
-        
-        Lookups_policies_api.assertThatResponse().matchesSchema("Lookups_policies_schema.json").perform();
-        
-        System.out.println("Lookups policies Response Body  : - "+Lookups_policies_ResponseBody+"   -E");
-
-        long responseTime = Lookups_policies_api.getResponse().getTime();
-        SHAFT.Validations.verifyThat().number(responseTime).isGreaterThanOrEquals(1.1).perform();
-        SHAFT.Validations.verifyThat().number(responseTime).isLessThanOrEquals(10000).perform(); 
     }
  
 }
---
+++ src/test/java/SteeringCompanyAPI_TestCases/Test_Policies_Cases.java
@@ -0,0 +1,42 @@
+package SteeringCompanyAPI_TestCases;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.shaft.tools.io.ExcelFileManager;
+
+import SteeringCompanyAPIs.Lookups_API;
+import SteeringCompanyAPIs.Token_API;
+
+public class Test_Policies_Cases {
+    
+    ExcelFileManager testDataReader ;
+    String UserName,Password,InValidUserName,InValidPassword;
+   //     String EmptyUserName=testDataReader.getCellData("TokenAPI_TestData","UserName","Data3");
+  //      String EmptyPassword=testDataReader.getCellData("TokenAPI_TestData","Password","Data3");
+     @BeforeClass
+     public void Setup_data() {
+     testDataReader = new ExcelFileManager("SteeringCompanyAPI_TestData/SteeringCompanyAPI_TestData.xlsx");
+     UserName=testDataReader.getCellData("TokenAPI_TestData","UserName","Data1");
+     Password=testDataReader.getCellData("TokenAPI_TestData","Password","Data1");
+     InValidUserName=testDataReader.getCellData("TokenAPI_TestData","UserName","Data2");
+     InValidPassword=testDataReader.getCellData("TokenAPI_TestData","Password","Data2");  
+     }
+   
+     
+     @Test(description = "TC001 - Perform Get all Policies API with valid user name and password")
+     public void Valid_GET_all_cities_Lookups_Rq_TC() {
+         Token_API Token_TC=new Token_API();
+         Token_TC.POST_Valid_TOKEN_Rq(UserName,Password);
+         Token_TC.Check_Token_Valid_status_Code_Response();
+         String Token =Token_TC.Get_Valid_Access_Token();
+         
+         Lookups_API Lookups_TC=new Lookups_API();
+         Lookups_TC.GET_Valid_all_cities_Lookups_Rq(Token);
+         Lookups_TC.Check_Valid_cities_Lookups_status_Code_Response();
+         Lookups_TC.Check_Valid_cities_Lookups_Response_Time();
+         Lookups_TC.Check_cities_Lookups_Valid_Content();
+         Lookups_TC.Check_cities_Lookups_Response_Valid_Schema();
+             }
+
+}
