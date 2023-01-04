54e0a231e43af5a82117b89b9510fef3de0d556b
Heshamabdo21 <heshamabdo21@gmail.com>
2023-01-04 12:52:15 +0200

    test

--- src/main/java/SteeringCompanyAPIs/Policies_API.java
+++ src/main/java/SteeringCompanyAPIs/Policies_API.java
@@ -6,12 +6,15 @@ import com.shaft.tools.io.ExcelFileManager;
 import com.shaft.api.*;
 import com.shaft.api.RequestBuilder.AuthenticationType;
 
+import com.sun.net.httpserver.Authenticator;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 
 public class Policies_API {
@@ -19,13 +22,13 @@ public class Policies_API {
     String BaseURL = testDataReader.getCellData("API_Data","Steering_Base_URL","Path");
 
     //String BaseURL = "https://api-demo.np.transporticonline.com/steeringcompanies/v1" ;
-   
+
     Response All_Policies_Response;
     SHAFT.API All_Policies_api;
    // String All_Policies_Path = "/policies";
     String All_Policies_Path = testDataReader.getCellData("API_Data","GetAllPolicies","Path");
-  
-    public void GET_All_Policies_Rq(String TokenValue) {
+  /////////////////////////// Methods for Get All Policies ///////////////////////
+    public void GET_all_Policies_Rq(String TokenValue) {
      //   String Lookup_policies_Path = "/policies";
     	 All_Policies_api = new SHAFT.API(BaseURL); 
     	 All_Policies_api.get(All_Policies_Path).
@@ -33,12 +36,52 @@ public class Policies_API {
     	addHeader("Authorization", "Bearer " + TokenValue).perform();     
          All_Policies_Response = All_Policies_api.getResponse();
     }
-    
+
+    public void GET_all_Policies_Path_by_parameter_Query_Rq(String TokenValue,String PageSize,String PageNumber) {
+        //  String Lookup_policies_Path = "/policies";
+        All_Policies_api = new SHAFT.API(BaseURL);
+        List<List<Object>> parameters = Arrays.asList(Arrays.asList("size", PageSize),
+                Arrays.asList("page",PageNumber));
+        All_Policies_api.get(All_Policies_Path).
+                setAuthentication("", "", AuthenticationType.NONE).
+                setParameters(parameters, RestActions.ParametersType.QUERY).
+                addHeader("Authorization", "Bearer " + TokenValue).perform();
+        All_Policies_Response = All_Policies_api.getResponse();
+
+    }
+
+    public void GET_all_Policies_With_Missing_Token_Rq() {
+        // String Lookup_cities_Path = "/lookups/cities";
+        All_Policies_api = new SHAFT.API(BaseURL);
+        All_Policies_api.get(All_Policies_Path).
+                setAuthentication("","", AuthenticationType.NONE).
+                setTargetStatusCode(401).perform();
+              //  addHeader("Authorization", "Bearer " ).perform();
+
+        All_Policies_Response = All_Policies_api.getResponse();
+    }
+
+    public void GET_all_Policies_With_InValid_Token_Rq(String TokenValue) {
+        // String Lookup_cities_Path = "/lookups/cities";
+
+        All_Policies_api = new SHAFT.API(BaseURL);
+        All_Policies_api.get(All_Policies_Path).
+                setAuthentication("","", AuthenticationType.NONE).
+                setTargetStatusCode(401).
+                addHeader("Authorization", "Bearer "+ TokenValue ).perform();
+
+        All_Policies_Response = All_Policies_api.getResponse();
+    }
+
     public void Check_Valid_policies_status_Code_Response(){
         SHAFT.Validations.assertThat().number(All_Policies_Response.getStatusCode()).isEqualTo(200).perform();
     }
-    
-    public void Check_Valid_policies_Response_Time() {
+
+    public void Check_Policies_status_Code_Unauthorized_Response(){
+        SHAFT.Validations.assertThat().number(All_Policies_Response.getStatusCode()).isEqualTo(401).perform();
+    }
+
+    public void Check_policies_Response_Time() {
         SHAFT.Validations.verifyThat().number(All_Policies_Response.getTime()).isGreaterThanOrEquals(1.1).perform();
         SHAFT.Validations.verifyThat().number(All_Policies_Response.getTime()).isLessThanOrEquals(10000).perform();
     }
@@ -51,20 +94,60 @@ public class Policies_API {
     
     public void Check_policies_Response_Valid_Schema() {
         All_Policies_api.assertThatResponse().matchesSchema(testDataReader.getCellData("API_Data","GetAllPolicies","Schema")).perform();
-     
     }
-   
-    public void GET_All_Policies_Path_by_parameter_Query_Rq(String TokenValue,String PageSize,String PageNumber) {        
-      //  String Lookup_policies_Path = "/policies";
-        All_Policies_api = new SHAFT.API(BaseURL);
-    	List<List<Object>> parameters = Arrays.asList(Arrays.asList("size", PageSize), 
-    			Arrays.asList("page",PageNumber));
-    	All_Policies_api.get(All_Policies_Path).
-    	setAuthentication("", "", AuthenticationType.NONE).
-    	setParameters(parameters, RestActions.ParametersType.QUERY).
-    	addHeader("Authorization", "Bearer " + TokenValue).perform();
-    	All_Policies_Response = All_Policies_api.getResponse();
-        
+
+    public void Check_policies_Response_Unauthorized_Schema() {
+        All_Policies_api.assertThatResponse().matchesSchema(testDataReader.getCellData("API_Data","UnAuthorized","Path")).perform();
     }
- 
+
+    /////////////////////// Add Cancel Policy Methods ///////////////////
+    String Cancel_Policy_Path = testDataReader.getCellData("API_Data","AddCancellationPolicies","Path");
+    String Tax_Policy_Path = testDataReader.getCellData("API_Data","AddTaxPolicies","Path");
+    String Payment_Policy_Path = testDataReader.getCellData("API_Data","AddPaymentPolicies","Path");
+    String General_Policy_Path = testDataReader.getCellData("API_Data","AddGeneralPolicies","Path");
+    String Usage_Policy_Path = testDataReader.getCellData("API_Data","AddUsagePolicies","Path");
+
+    Response Policy_Response;
+    SHAFT.API Policy_api;
+    public void Add_Policy_Rq(String TokenValue, @NotNull  Object data[]) {
+        Random random=new Random();
+        int x=random.nextInt(10000);
+        Policy_api = new SHAFT.API(BaseURL);
+        String Add_Cancel_PolicyBody="{\n"+
+                "    \"nameArabic\": \""+data[1]+x+"\",\n" +
+                "    \"nameEnglish\": \""+data[2]+x+"\",\n" +
+                "    \"descriptionArabic\": \""+data[3]+x+"\",\n" +
+                "    \"descriptionEnglish\": \""+data[4]+x+"\",\n" +
+                "    \"chargeUnit\": \""+data[5]+"\",\n" +
+                "    \"deadline\": \""+data[6]+"\",\n" +
+                "    \"chargeType\": \""+data[7]+"\",\n" +
+                "    \"chargeValue\": \""+data[8]+"\",\n" +
+                "    \"id\": \""+data[9]+"\"\n"+
+                "}";
+        Policy_api.post(Cancel_Policy_Path).
+                setRequestBody(Add_Cancel_PolicyBody).
+                setTargetStatusCode(201).
+                setContentType(ContentType.JSON).
+                setAuthentication("","", AuthenticationType.NONE).
+                addHeader("Authorization", "Bearer " + TokenValue).perform();
+        Policy_Response = Policy_api.getResponse();
+    }
+
+    public void Check_Valid_Add_policies_status_Code_Response(){
+        SHAFT.Validations.assertThat().number(Policy_Response.getStatusCode()).isEqualTo(201).perform();
+    }
+    public void Check_Add_policy_Response_Valid_Schema() {
+        Policy_api.assertThatResponse().matchesSchema(testDataReader.getCellData("API_Data","AddCancellationPolicies","Schema")).perform();
+    }
+
+    public void Check_Add_Policy_Response_Time() {
+        SHAFT.Validations.verifyThat().number(Policy_Response.getTime()).isGreaterThanOrEquals(1.1).perform();
+        SHAFT.Validations.verifyThat().number(Policy_Response.getTime()).isLessThanOrEquals(10000).perform();
+    }
+
+    public void Check_Add_policy_Response_Unauthorized_Schema() {
+        Policy_api.assertThatResponse().matchesSchema(testDataReader.getCellData("API_Data","UnAuthorized","Path")).perform();
+    }
+
+
 }
--- src/test/java/SteeringCompanyAPI_TestCases/Test_Policies_Cases.java
+++ src/test/java/SteeringCompanyAPI_TestCases/Test_Policies_Cases.java
@@ -1,5 +1,6 @@
 package SteeringCompanyAPI_TestCases;
 
+import io.qameta.allure.*;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
@@ -12,54 +13,31 @@ import SteeringCompanyAPIs.Token_API;
 public class Test_Policies_Cases {
     
     ExcelFileManager testDataReader ;
-    String UserName,Password,InValidUserName,InValidPassword;
-   //     String EmptyUserName=testDataReader.getCellData("TokenAPI_TestData","UserName","Data3");
-  //      String EmptyPassword=testDataReader.getCellData("TokenAPI_TestData","Password","Data3");
+    String UserName,Password;
      @BeforeClass
+     ///////// Read Data for Token API ///////////////////////////////
      public void Setup_data() {
      testDataReader = new ExcelFileManager("SteeringCompanyAPI_TestData/SteeringCompanyAPI_TestData.xlsx");
      UserName=testDataReader.getCellData("TokenAPI_TestData","UserName","Data1");
      Password=testDataReader.getCellData("TokenAPI_TestData","Password","Data1");
-     InValidUserName=testDataReader.getCellData("TokenAPI_TestData","UserName","Data2");
-     InValidPassword=testDataReader.getCellData("TokenAPI_TestData","Password","Data2");  
      }
 
-     @DataProvider (name = "Valid_data_for_Cancel_Policy")
+     //////////////////Test Cases for Get All Active Policies API ////////////////////////////
 
-     public Object[][] Valid_Cancel_Policy(){
-        // int Rows=testDataReader.();
-         int Columns=testDataReader.getLastColumnNumber("CancelPolicy_TestData");
-         Object data[][]=new Object[2][ Columns];
-         for (int i=0;i<2;i++)
-         {
-             data[i][0]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"TC_Type");
-             data[i][1]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"nameArabic");;
-             data[i][2]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"nameEnglish");;
-             data[i][3]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"descriptionArabic");;
-             data[i][4]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"descriptionEnglish");;
-             data[i][5]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"chargeUnit");;
-             data[i][6]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"deadline");;
-             data[i][7]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"chargeType");;
-             data[i][8]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"chargeValue");;
-             data[i][9]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"id");;
-             data[i][10]=testDataReader.getCellData("CancelPolicy_TestData","TC_Valid_"+i,"ExpectedResult");;
-         }
-         return data;
-     }
-     @Test(description = "TC001 - Perform Get all Policies API with valid user name and password")
-     public void Valid_GET_all_Policies_Rq_TC() {
+    @Test(description = "TC001 - Perform Get all Policies API with valid user name and password")
+    public void Valid_GET_all_Policies_Rq_TC() {
          Token_API Token_TC=new Token_API();
          Token_TC.POST_Valid_TOKEN_Rq(UserName,Password);
          Token_TC.Check_Token_Valid_status_Code_Response();
          String Token =Token_TC.Get_Valid_Access_Token();
 
          Policies_API GetAllPolicies_TC=new Policies_API();
-         GetAllPolicies_TC.GET_All_Policies_Rq(Token);
+         GetAllPolicies_TC.GET_all_Policies_Rq(Token);
          GetAllPolicies_TC.Check_Valid_policies_status_Code_Response();
-        // GetAllPolicies_TC.Check_Valid_policies_Response_Time();
+         GetAllPolicies_TC.Check_policies_Response_Time();
          GetAllPolicies_TC.Check_policies_Valid_Content();
          GetAllPolicies_TC.Check_policies_Response_Valid_Schema();
-             }
+     }
 
     @Test(description = "TC002 - Perform Get all Policies API with pagination with valid user name and password")
     public void Valid_GET_all_Policies_by_Qry_Rq_TC() {
@@ -69,11 +47,78 @@ public class Test_Policies_Cases {
         String Token =Token_TC.Get_Valid_Access_Token();
 
         Policies_API GetAllPolicies_TC=new Policies_API();
-        GetAllPolicies_TC.GET_All_Policies_Path_by_parameter_Query_Rq(Token,"3","0");
+        GetAllPolicies_TC.GET_all_Policies_Path_by_parameter_Query_Rq(Token,"3","0");
         GetAllPolicies_TC.Check_Valid_policies_status_Code_Response();
-        GetAllPolicies_TC.Check_Valid_policies_Response_Time();
+        GetAllPolicies_TC.Check_policies_Response_Time();
         GetAllPolicies_TC.Check_policies_Valid_Content();
         GetAllPolicies_TC.Check_policies_Response_Valid_Schema();
     }
+
+    @Test(description = "TC003 - Perform Get all Policies API with missing Token")
+    public void Invalid_GET_all_Policies_with_missing_Token_TC() {
+        Token_API Token_TC=new Token_API();
+        Token_TC.POST_Valid_TOKEN_Rq(UserName,Password);
+        Token_TC.Check_Token_Valid_status_Code_Response();
+        String Token =Token_TC.Get_Valid_Access_Token();
+
+        Policies_API GetAllPolicies_TC=new Policies_API();
+        GetAllPolicies_TC.GET_all_Policies_With_Missing_Token_Rq();
+        GetAllPolicies_TC.Check_Policies_status_Code_Unauthorized_Response();
+        GetAllPolicies_TC.Check_policies_Response_Time();
+        GetAllPolicies_TC.Check_policies_Response_Unauthorized_Schema();
+    }
+
+    @Test(description = "TC004 - Perform Get all Policies API with invalid or expired Token")
+    public void Invalid_GET_all_Policies_with_invalid_Token_TC() {
+        Token_API Token_TC=new Token_API();
+        Token_TC.POST_Valid_TOKEN_Rq(UserName,Password);
+        Token_TC.Check_Token_Valid_status_Code_Response();
+        String Token =Token_TC.Get_Valid_Access_Token();
+
+        Policies_API GetAllPolicies_TC=new Policies_API();
+        GetAllPolicies_TC.GET_all_Policies_With_InValid_Token_Rq("123");
+        GetAllPolicies_TC.Check_Policies_status_Code_Unauthorized_Response();
+        GetAllPolicies_TC.Check_policies_Response_Time();
+        //GetAllPolicies_TC.Check_policies_Response_Unauthorized_Schema(); // There is no response content
+    }
+
+    /////////////////////// Test Case for Add Cancel Policies //////////////////////////////////////////
+    @DataProvider (name = "Valid_data_for_Cancel_Policy")
+    public Object[][] Valid_Cancel_Policy(){
+        Object data[][]=new Object[2][ 11];
+        for (int i=0;i<2;i++)
+        {
+            data[i][0]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"TC_Type");
+            data[i][1]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"nameArabic");;
+            data[i][2]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"nameEnglish");;
+            data[i][3]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"descriptionArabic");;
+            data[i][4]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"descriptionEnglish");;
+            data[i][5]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"chargeUnit");;
+            data[i][6]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"deadline");;
+            data[i][7]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"chargeType");;
+            data[i][8]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"chargeValue");;
+            data[i][9]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"id");;
+            data[i][10]=testDataReader.getCellData("Policy_TestData","Cancel_Valid_"+(i+1),"ExpectedResult");;
+        }
+        return data;
+    }
+
+    @Test(description = "TC005 - Perform Add Valid Cancel Policy",dataProvider = "Valid_data_for_Cancel_Policy")
+    @Story("CRUD Operations")
+    @Severity(SeverityLevel.CRITICAL)
+    public void Valid_Add_Cancel_Policy_TC(Object data[]){
+        Token_API Token_TC=new Token_API();
+        Token_TC.POST_Valid_TOKEN_Rq(UserName,Password);
+        Token_TC.Check_Token_Valid_status_Code_Response();
+        String Token =Token_TC.Get_Valid_Access_Token();
+
+        Policies_API AddCancelPolicies_TC=new Policies_API();
+        AddCancelPolicies_TC.Add_Policy_Rq(Token,data);
+        AddCancelPolicies_TC.Check_Valid_Add_policies_status_Code_Response();
+        AddCancelPolicies_TC.Check_Add_Policy_Response_Time();
+       // AddCancelPolicies_TC.Check_policies_Valid_Content();
+        AddCancelPolicies_TC.Check_Add_policy_Response_Valid_Schema();
+    }
+
 
 }
