262adbaf2aa878f754be110239e6bcee46253028
Heshamabdo21 <heshamabdo21@gmail.com>
2023-03-06 09:27:40 +0200

    test

--- src/test/java/SteeringCompanyAPI_TestCases/Test_PeriodProgram_Cases.java
+++ src/test/java/SteeringCompanyAPI_TestCases/Test_PeriodProgram_Cases.java
@@ -20,62 +20,65 @@ import org.json.JSONException;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.Objects;
+
+import static org.testng.Assert.assertNotNull;
 @SuppressWarnings("ALL")
 public class Test_PeriodProgram_Cases {
 
-    ExtraExcelFun testDataReader ;
+    ExtraExcelFun testDataReader;
     ExtraExcelFun testDataReader2;
 
-    String UserName,Password;
-    ///////// Read Data for Token API ///////////////////////////////
-    @BeforeClass
-     public void Setup_data() {
-    // testDataReader = new ExcelFileManager("SteeringCompanyAPI_TestData/SteeringCompanyAPI_TestData.xlsx");
-     testDataReader2 = new ExtraExcelFun("SteeringCompanyAPI_TestData/SteeringCompanyAPI_TestData.xlsx");
-     UserName=testDataReader2.getCellData("TokenAPI_TestData","UserName","Data1");
-     Password=testDataReader2.getCellData("TokenAPI_TestData","Password","Data1");
-        zone = org.joda.time.DateTimeZone.forID("Asia/Riyadh");
-        GJChronologydate = GJChronology.getInstance(zone);
-        Today = new LocalDate(GJChronologydate);
-     }
+    String UserName, Password;
     DateTimeZone zone;
     Chronology GJChronologydate;
     LocalDate Today;
-    Object[] FirstValidCreationData,FirstValidPeriodProgramTemplatesData;
-public String SharedSteps_GetToken(){
-    Token_API Token_TC=new Token_API();
-    Token_TC.POST_Valid_TOKEN_Rq(UserName,Password);
-    Token_TC.Check_Token_Valid_status_Code_Response();
-    String Token =Token_TC.Get_Valid_Access_Token();
-    return Token;
-}
-    @Test(description = "PeriodProgram- Get Valid Today Creational Period",priority = 0)
+    Object[] FirstValidCreationData, FirstValidPeriodProgramTemplatesData;
+
+    ///////// Read Data for Token API ///////////////////////////////
+    @BeforeClass
+    public void Setup_data() {
+        // testDataReader = new ExcelFileManager("SteeringCompanyAPI_TestData/SteeringCompanyAPI_TestData.xlsx");
+        testDataReader2 = new ExtraExcelFun("SteeringCompanyAPI_TestData/SteeringCompanyAPI_TestData.xlsx");
+        UserName = testDataReader2.getCellData("TokenAPI_TestData", "UserName", "Data1");
+        Password = testDataReader2.getCellData("TokenAPI_TestData", "Password", "Data1");
+        zone = org.joda.time.DateTimeZone.forID("Asia/Riyadh");
+        GJChronologydate = GJChronology.getInstance(zone);
+        Today = new LocalDate(GJChronologydate);
+    }
+
+    public String SharedSteps_GetToken() {
+        Token_API Token_TC = new Token_API();
+        Token_TC.POST_Valid_TOKEN_Rq(UserName, Password);
+        Token_TC.Check_Token_Valid_status_Code_Response();
+       // String Token = Token_TC.Get_Valid_Access_Token();
+        return Token_TC.Get_Valid_Access_Token();
+    }
+
+    @Test(description = "PeriodProgram- Get Valid Today Creational Period", priority = 0)
     @Story("Perpare Date for PeriodProgram")
     @Severity(SeverityLevel.CRITICAL)
     public void Get_Valid_Today_Creational_TC() throws JSONException {
-        String Token=SharedSteps_GetToken().toString();
-        CreationalPeriods_API GetAllCreationalPeriod_TC=new CreationalPeriods_API();
+        String Token = SharedSteps_GetToken().toString();
+        CreationalPeriods_API GetAllCreationalPeriod_TC = new CreationalPeriods_API();
         GetAllCreationalPeriod_TC.Get_Valid_all_CreationalPeriods_Rq(Token);
-        FirstValidCreationData=GetAllCreationalPeriod_TC.Get_Valid_Today_CreationalPeriods();
+        FirstValidCreationData = GetAllCreationalPeriod_TC.Get_Valid_Today_CreationalPeriods();
         // FirstValidPeriodProgramTemplatesData;
         PeriodProgramTemplates_API GetAllPeriodProgramTemplate_TC = new PeriodProgramTemplates_API();
-        if(FirstValidCreationData!=null) {
+        if (Objects.nonNull(FirstValidCreationData)) {
             GetAllPeriodProgramTemplate_TC.Get_Valid_PeriodProgramTemplates_by_id_Rq(Token, FirstValidCreationData[2].toString());
-            FirstValidPeriodProgramTemplatesData=GetAllPeriodProgramTemplate_TC.Get_Valid_PeriodProgramTemplates_by_id();
-        }else{
-            SteeringCompanyQry.UpdateLastCreationalPeriod(Today,Today.plusDays(1));
+            FirstValidPeriodProgramTemplatesData = GetAllPeriodProgramTemplate_TC.Get_Valid_PeriodProgramTemplates_by_id();
+        } else {
+            SteeringCompanyQry.UpdateLastCreationalPeriod(Today, Today.plusDays(1));
             GetAllCreationalPeriod_TC.Get_Valid_all_CreationalPeriods_Rq(Token);
-            FirstValidCreationData=GetAllCreationalPeriod_TC.Get_Valid_Today_CreationalPeriods();
+            FirstValidCreationData = GetAllCreationalPeriod_TC.Get_Valid_Today_CreationalPeriods();
             GetAllPeriodProgramTemplate_TC.Get_Valid_PeriodProgramTemplates_by_id_Rq(Token, FirstValidCreationData[2].toString());
-            FirstValidPeriodProgramTemplatesData=GetAllPeriodProgramTemplate_TC.Get_Valid_PeriodProgramTemplates_by_id();
-        }
-        for (int i=0;i<FirstValidCreationData.length;i++){
-            System.out.println("FirstValidCreationData ["+i+"] ::: "+FirstValidCreationData[i].toString());
+            FirstValidPeriodProgramTemplatesData = GetAllPeriodProgramTemplate_TC.Get_Valid_PeriodProgramTemplates_by_id();
         }
 
-        for (int i=0;i<FirstValidPeriodProgramTemplatesData.length;i++){
-            System.out.println("FirstValidPeriodProgramTemplatesData ["+i+"] ::: "+FirstValidPeriodProgramTemplatesData[i].toString());
-        }
+        Arrays.stream(FirstValidPeriodProgramTemplatesData).forEach(data -> System.out.println("FirstValidPeriodProgramTemplatesData ::: " + data));
+        Arrays.stream(FirstValidCreationData).forEach(data -> System.out.println("FirstValidCreationData ::: " + data));
     }
 
     /////////////////////// Test Case for Add PeriodPrograms //////////////////////////////////////////
